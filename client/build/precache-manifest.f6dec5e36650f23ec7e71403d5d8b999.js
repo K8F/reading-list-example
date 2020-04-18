d1d9d20af3b68c2e83cebe3a85ff9bf7840be527
Kate Foust <kate.e.foust@gmail.com>
2020-04-18 11:31:48 -0700

    npm run build

---
+++ client/build/precache-manifest.f6dec5e36650f23ec7e71403d5d8b999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a98caa3d60d9093615c8",
+    "url": "/static/css/main.68f50191.chunk.css"
+  },
+  {
+    "revision": "a98caa3d60d9093615c8",
+    "url": "/static/js/main.65d55ed2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "219f29db5f10382fa35f",
+    "url": "/static/js/2.600514d9.chunk.js"
+  },
+  {
+    "revision": "96e3bf709e7f53bddaa7685d742d057f",
+    "url": "/index.html"
+  }
+];
